0a4dedbb4296f07c82f1b9fe716c08b718a3a60a
ZoeLeBlanc <zgleblanc@gmail.com>
2024-04-03 13:10:46 -0500

    updates to R scripts

--- missing-data/logbooks_table.R
+++ missing-data/logbooks_table.R
@@ -3,10 +3,12 @@ list.of.packages <- c("ggplot2", "glue", "gt", "tidyverse", "webshot", "palettee
 
 # install required packages, if necessary, and load them ----
 {
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) install.packages(new.packages)
-  lapply(list.of.packages, require, character.only = TRUE)
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+  if (length(new.packages)) install.packages(new.packages)
 }
+
+# Instead of using require, we'll use library to load the packages
+lapply(list.of.packages, library, character.only = TRUE)
 
 skipped <- read_csv('skipped_df.csv')
 

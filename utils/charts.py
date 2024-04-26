fd1b657a26a01b03056aa932231e6f0b04aac929
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-04-26 16:45:08 -0400

    Rename chart utils

--- utils/charts.py
+++ utils/charts.py
@@ -1,10 +1,14 @@
 import vl_convert as vlc
 import altair as alt
 
-def save_chart(chart, filename, scale_factor=1):
-    '''
+
+# TODO: standard chart options for font, title layout, etc. could be set here
+
+
+def save_altair_chart(chart, filename, scale_factor=1):
+    """
     Save an Altair chart using vl-convert
-    
+
     Parameters
     ----------
     chart : altair.Chart
@@ -14,12 +18,14 @@ def save_chart(chart, filename, scale_factor=1):
     scale_factor: int or float
         The factor to scale the image resolution by.
         E.g. A value of `2` means two times the default resolution.
-    '''
-    with alt.data_transformers.enable("default"), alt.data_transformers.disable_max_rows():
-        if filename.split('.')[-1] == 'svg':
+    """
+    with alt.data_transformers.enable(
+        "default"
+    ), alt.data_transformers.disable_max_rows():
+        if filename.split(".")[-1] == "svg":
             with open(filename, "w") as f:
                 f.write(vlc.vegalite_to_svg(chart.to_dict()))
-        elif filename.split('.')[-1] == 'png':
+        elif filename.split(".")[-1] == "png":
             with open(filename, "wb") as f:
                 f.write(vlc.vegalite_to_png(chart.to_dict(), scale=scale_factor))
         else:
